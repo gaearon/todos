e9f54d9db357265e12e7fb07ed3b3e4850ecc542
Dan Abramov <dan.abramov@gmail.com>
2016-05-02 19:20:58 +0100

    Init

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from './Footer';
+import AddTodo from './AddTodo';
+import VisibleTodoList from './VisibleTodoList';
+
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+);
+
+export default App;
