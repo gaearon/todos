cfb84444c1852b1ca78bfb3bce5aade8d1ef366c
Dan Abramov <dan.abramov@gmail.com>
2016-05-11 13:06:22 +0100

    Wrapping dispatch() to log actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,9 +3,32 @@ import throttle from 'lodash/throttle';
 import todoApp from './reducers';
 import { loadState, saveState } from './localStorage';
 
+const addLoggingToDispatch = (store) => {
+  /* eslint-disable no-console */
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+  /* eslint-enable no-console */
+};
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(todoApp, persistedState);
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   store.subscribe(throttle(() => {
     saveState({
