199de271221c331725e07159679c7cfbba21b901
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-02-10 10:47:10 -0600

    Initialize routing via browserHistory

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './App';
 
 const Root = ({ store }) => (
 	<Provider store={store}>
-		<App />
+		<Router history={browserHistory}>
+			<Route path='/(:filter)' component={App} />
+		</Router>
 	</Provider>
 );
 
 export default Root;
+
+// (:filter) is wrapped in parens b/c it is OPTIONAL
