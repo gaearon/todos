a01abaf4d1ac2ee43905a3369ec963479846f95e
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-02-21 09:11:14 -0600

    Create VisibleTodoList component to overwrite lifecycle hook

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,13 +1,30 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router'; // Takes a component and returns a new component that injects router related props (like params) inside your component
 import { toggleTodo } from '../actions';
 import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
+import { fetchTodos } from '../api';
 
+class VisibleTodoList extends Component { // The only reason we create a component here is b/c
+	componentDidMount() { // you can't overwrite the lifecycle hooks of a generated component
+		fetchTodos(this.props.filter).then(todos =>
+			console.log(this.props.filter, todos);
+		);
+	}
 
-const mapStateToProps = (state, { params }) => ({ // { params } === ownProps.params
-    todos: getVisibleTodos(state, params.filter || 'all'), // now we can just pass the state b/c getVisibleTodos encapsulates all the knowledge
-});                               // about the application state shape
+	render() {
+		return <TodoList {...this.props} />;
+	}
+}
+
+const mapStateToProps = (state, { params }) => { // { params } === ownProps.params
+	const filter = params.filter || 'all';
+	return {
+		todos: getVisibleTodos(state, filter), // now we can just pass the state b/c getVisibleTodos encapsulates all the knowledge
+        filter,                      // about the application state shape
+	}
+};
 
 // const mapDispatchToProps = (dispatch) => ({
 //     onTodoClick: (id) => { // when the args for the cb match the args for the action creator exactly
@@ -15,10 +32,10 @@ const mapStateToProps = (state, { params }) => ({ // { params } === ownProps.par
 //     },
 // });
 
-const VisibleTodoList = withRouter(connect(
+VisibleTodoList = withRouter(connect(
   mapStateToProps,
   /*mapDispatchToProps*/
   { onTodoClick: toggleTodo } // obj contains the cb func mapped to the action creator func we want to inject
-)(TodoList));
+)(VisibleTodoList));
 
 export default VisibleTodoList;
