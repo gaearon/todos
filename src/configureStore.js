e3208806eb5853bfb9d10b52fc44e944769b66ab
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-02-21 22:48:18 -0600

    Comment out and import promise and createLogger middlewares

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,50 +1,48 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
-const logger = (store) => (next) => {
-	if (!console.group) {
-		return next;
-	}
-	return (action) => {
-		console.group(action.type);
-		console.log('%c prev state', 'color: gray', store.getState());
-		console.log('%c action', 'color: blue', action);
-		const returnValue = next(action);
-		console.log('%c next state', 'color: green', store.getState());
-		console.groupEnd(action.type);
-		return returnValue;
-	};
-};
+// MANY MIDDLEWARES ARE AVAILABLE FROM NPM
+// const logger = (store) => (next) => {
+// 	if (!console.group) {
+// 		return next;
+// 	}
+// 	return (action) => {
+// 		console.group(action.type);
+// 		console.log('%c prev state', 'color: gray', store.getState());
+// 		console.log('%c action', 'color: blue', action);
+// 		const returnValue = next(action);
+// 		console.log('%c next state', 'color: green', store.getState());
+// 		console.groupEnd(action.type);
+// 		return returnValue;
+// 	};
+// };
 
-const promise = (store) => (next) => (action) => {
-	if (typeof action.then === 'function') {
-		return action.then(next);
-	}
-	return next(action);
-};
+// const promise = (store) => (next) => (action) => {
+// 	if (typeof action.then === 'function') {
+// 		return action.then(next);
+// 	}
+// 	return next(action);
+// };
 
-const wrapDispatchWithmiddlewares = (store, middlewares) => {
-	middlewares.slice().reverse().forEach(middleware =>
-		store.dispatch = middleware(store)
-	);
-};
+// const wrapDispatchWithmiddlewares = (store, middlewares) => {
+// 	middlewares.slice().reverse().forEach(middleware =>
+// 		store.dispatch = middleware(store)(store.dispatch)
+// 	);
+// };
 
 const configureStore = () => {
 	const middlewares = [promise];
-	const store = createStore(
-		todoApp
+	if (process.env.NODE_ENV !== 'production') {
+		middlewares.push(createLogger());
+	}
+	return createStore(
+		todoApp,
+		applyMiddleware(...middlewares) // This last arg to createStore is called an enhancer, it's optional
 		 // Whatever is passed as the second arg to createStore will end up as the state for the root reducer
 	); // You can use the 2nd arg to hydrate the redux store with persisted data because it was obtained from redux
 	// and therefor doesn't break encapsulation of reducers
-	console.log(store.getState());
-
-	if (process.env.NODE_ENV !== 'production') {
-		middlewares.push(logger);
-	}
-
-	wrapDispatchWithmiddlewares(store, middlewares);
-
-	return store;
 };
 
 export default configureStore; // Its good to export it instead of having it store itself
