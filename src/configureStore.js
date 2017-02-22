d741eb7e43734090ca576b6d6bfd8e8d57ed6a3a
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-02-21 22:15:04 -0600

    curryify middleware funcs and reverse the of flow wrapper

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,42 +1,36 @@
 import { createStore } from 'redux';
 import todoApp from './reducers';
 
-const addLoggingToDispatch = (store) => {
-	return (next) => {
-		if (!console.group) {
-			return next;
-		}
-		return (action) => {
-			console.group(action.type);
-			console.log('%c prev state', 'color: gray', store.getState());
-			console.log('%c action', 'color: blue', action);
-			const returnValue = next(action);
-			console.log('%c next state', 'color: green', store.getState());
-			console.groupEnd(action.type);
-			return returnValue;
-		};
+const logger = (store) => (next) => {
+	if (!console.group) {
+		return next;
+	}
+	return (action) => {
+		console.group(action.type);
+		console.log('%c prev state', 'color: gray', store.getState());
+		console.log('%c action', 'color: blue', action);
+		const returnValue = next(action);
+		console.log('%c next state', 'color: green', store.getState());
+		console.groupEnd(action.type);
+		return returnValue;
 	};
 };
 
-const addPromiseSupportToDispatch = (store) => {
-	return (next) => {
-		return (action) => {
-			if (typeof action.then === 'function') {
-				return action.then(next);
-			}
-			return next(action);
-		};
-	};
+const promise = (store) => (next) => (action) => {
+	if (typeof action.then === 'function') {
+		return action.then(next);
+	}
+	return next(action);
 };
 
 const wrapDispatchWithmiddlewares = (store, middlewares) => {
-	middlewares.forEach(middleware =>
+	middlewares.slice().reverse().forEach(middleware =>
 		store.dispatch = middleware(store)
 	);
 };
 
 const configureStore = () => {
-	const middlewares = [];
+	const middlewares = [promise];
 	const store = createStore(
 		todoApp
 		 // Whatever is passed as the second arg to createStore will end up as the state for the root reducer
@@ -45,10 +39,8 @@ const configureStore = () => {
 	console.log(store.getState());
 
 	if (process.env.NODE_ENV !== 'production') {
-		middlewares.push(addLoggingToDispatch);
+		middlewares.push(logger);
 	}
-
-	middlewares.push(addPromiseSupportToDispatch);
 
 	wrapDispatchWithmiddlewares(store, middlewares);
 
