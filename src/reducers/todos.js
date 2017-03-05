3e413560b410bac270827b45bcf71b376b582816
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-03-04 18:32:09 -0600

    Update logic in byId reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,14 +1,13 @@
 import { combineReducers } from 'redux';
-import todo from './todo';
 
 const byId = (state = {}, action) => {
   switch (action.type) {
-    case 'ADD_TODO':
-    case 'TOGGLE_TODO':
-      return {
-        ...state,
-        [action.id]: todo(state[action.id], action),
-      };
+    case 'RECEIVE_TODOS':
+      const nextState = { ...state }; // This shallow copy corresponds to the lookup table
+      action.response.forEach(todo => {
+        nextState[todo.id] = todo;
+      });
+      return nextState;
     default:
       return state;
   }
