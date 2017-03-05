df03fede2819ea21da2c05bade5e25e77d3d4a2c
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-03-04 20:17:23 -0600

    Abstract logic from three reducers into one

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,58 +1,24 @@
 import { combineReducers } from 'redux';
+import byId, * as fromById from './byId';
 
-const byId = (state = {}, action) => {
-  switch (action.type) {
-    case 'RECEIVE_TODOS':
-      const nextState = { ...state }; // This shallow copy corresponds to the lookup table
-      action.response.forEach(todo => {
-        nextState[todo.id] = todo;
-      });
-      return nextState;
-    default:
+const createList = (filter) => {
+  return (state = [], action) => {
+    if (action.filter !== filter) {
       return state;
+    }
+    switch (action.type) {
+      case 'RECEIVE_TODOS':
+        return action.response.map(todo => todo.id);
+      default:
+        return state;
+    }
   }
 };
-
-const allIds = (state = [], action) => {
-  if (action.filter !== 'all') {
-    return state;
-  }
-  switch (action.type) {
-    case 'RECEIVE_TODOS':
-      return action.response.map(todo => todo.id);
-    default:
-      return state;
-  }
-};
-
-const activeIds = (state = [], action) => {
-  if (action.filter !== 'active') {
-    return state;
-  }
-  switch (action.type) {
-    case 'RECEIVE_TODOS':
-      return action.response.map(todo => todo.id);
-    default:
-      return state;
-  }
-};
-
-const completedIds = (state = [], action) => {
-  if (action.filter !== 'completed') {
-    return state;
-  }
-  switch (action.type) {
-    case 'RECEIVE_TODOS':
-      return action.response.map(todo => todo.id);
-    default:
-      return state;
-  }
-}
 
 const idsByFilter = combineReducers({
-  all: allIds,
-  active: activeIds,
-  completed: completedIds,
+  all: createList('all'),
+  active: createList('active'),
+  completed: createList('completed'),
 });
 
 const todos = combineReducers({
