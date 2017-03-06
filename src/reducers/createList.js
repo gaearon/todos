44c1889649bcc641f079c412abf87ab1068bbefe
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-03-06 10:15:54 -0600

    Create reducer for rendering loading for UI purposes

--- src/reducers/createList.js
+++ src/reducers/createList.js
@@ -1,5 +1,7 @@
+import { combineReducers } from '.redux';
+
 const createList = (filter) => {
-  return (state = [], action) => {
+  const ids = (state = [], action) => {
     if (action.filter !== filter) {
       return state;
     }
@@ -9,9 +11,29 @@ const createList = (filter) => {
       default:
         return state;
     }
-  }
+  };
+
+  const isFetching = (state = false, action) => {
+    if (action.filter !== filter) {
+      return state;
+    }
+    switch (action.type) {
+      case 'REQUEST_TODOS':
+        return true;
+      case 'RECEIVE_TODOS':
+        return false;
+      default:
+        return state;
+    }
+  };
+
+  return combineReducers({
+    ids,
+    isFetching,
+  });
 };
 
 export default createList;
 
-export const getIds = (state) => state;
+export const getIds = (state) => state.ids;
+export const getIsFetching = (state) => state.isFetching;
