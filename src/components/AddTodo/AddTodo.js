6ff2c38e869d0fd5a891d9869f1f8b1d3b10bd67
Juan Pablo Calao Perez <jcalaop@gmail.com>
2019-06-04 14:42:28 -0500

    class components to function & Visible TodoList with effect

--- src/components/AddTodo/AddTodo.js
+++ src/components/AddTodo/AddTodo.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import *  as actions from '../../actions';
 import './AddTodo.css';
 
-class WrappedComponent extends Component {
-  render() {
-    const filter = this.props.match.params.filter || 'all';
+function WrappedComponent(props) {
+  const filter = props.match.params.filter || 'all';
+  let input;
 
-    let input;
-
-    return (
-      <div className="AddTodo">
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            if (!input.value.trim()) {
-              return;
-            }
-            this.props.addAsyncTodo(filter, input.value);
-            input.value = '';
-          }}
-        >
-          <input ref={node => { input = node; }} />
-          <button type="submit">
-            Add Todo
-          </button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="AddTodo">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
+          props.addAsyncTodo(filter, input.value);
+          input.value = '';
+        }}
+      >
+        <input ref={node => { input = node; }} />
+        <button type="submit">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  );
 }
 
 const AddTodo = compose(
