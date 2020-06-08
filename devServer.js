b53ffb457af76a336c7fbb74a2e95714544d1001
QuangMinh <quangminh.lenguyen.si@gmail.com>
2020-06-08 23:11:42 +0200

    change port devServer

--- devServer.js
+++ devServer.js
@@ -5,7 +5,7 @@ import config from './webpack.config.babel';
 import Express from 'express';
 
 const app = new Express();
-const port = 3000;
+const port = 3002;
 
 const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
